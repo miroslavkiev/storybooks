8de2ed2ba6d41891f9db037249db9eb4a9b8431c
Myroslav Kravchenko <kravch@gmail.com>
2018-08-13 18:44:55 +0300

    Fighting with HTML markup

--- app.js
+++ app.js
@@ -1,21 +1,62 @@
 const express = require('express');
+const exphbs = require('express-handlebars');
 const mongoose = require('mongoose');
 const passport = require('passport');
+const session = require('express-session');
+const cookieParser = require('cookie-parser');
+
+//Load user model
+require('./models/User');
 
 //Passport config
 require('./config/passport')(passport);
 
 //Load routes
 const auth = require('./routes/auth');
+const index = require('./routes/index');
+
+//Load keys
+const keys = require('./config/keys');
+
+//Map global promises
+mongoose.Promise = global.Promise;
+
+//Mongoose connect
+mongoose.connect(keys.mongoURI, {
+	useNewUrlParser:true
+})
+	.then(() => console.log('MongoDb connected'))
+	.catch(err => console.log(err));
 
 const app = express();
 
-app.get('/', (req, res) => {
-	res.send('It works');
+//Handlebars middleware
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+
+//Cookie parser middleware
+app.use(cookieParser());
+
+//express-session middleware
+app.use(session({
+	secret: 'secret',
+	resave: false,
+	saveUninitialized: false
+}));
+
+//Passport middleware
+app.use(passport.initialize());
+app.use(passport.session());
+
+//Set global vars
+app.use((req, res, next) => {
+	res.locals.user = req.user || null;
+	next();
 });
 
 //Use routes
 app.use('/auth', auth);
+app.use('/', index);
 
 const port = process.env.PORT || 80;
 
--- routes/auth.js
+++ routes/auth.js
@@ -6,7 +6,21 @@ router.get('/google', passport.authenticate('google', {scope: ['profile', 'email
 
 router.get('/google/callback',
 	passport.authenticate('google', { failureRedirect: '/' }), (req, res) => {
-		res.redirect('/dashboard');
+		res.redirect('/');
 	});
+
+router.get('/verify', (req, res) => {
+	if(req.user){
+		console.log(req.user);
+	} else {
+		console.log(`Not auth!`);
+	}
+});
+
+router.get('/logout', (req, res) => {
+	req.logout();
+	res.redirect('/');
+});
+
 
 module.exports = router;
