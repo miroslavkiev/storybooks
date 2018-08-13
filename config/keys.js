06601e2028a7028b5537b405373e36c64cfd0e2a
Myroslav Kravchenko <kravch@gmail.com>
2018-08-13 16:36:33 +0300

    Handled auth

---
+++ config/keys.js
@@ -0,0 +1,5 @@
+module.exports = {
+	mongoURI: 'mongodb://miroslav:miroslav25@ds219832.mlab.com:19832/sorybooks-dev',
+	googleClientID: '195106054458-ceaf2ep5urujqqccqvh93urslodk1i4v.apps.googleusercontent.com',
+	googleClientSecret: 'fquvAF1JnpjyOXldeqHhjopM'
+}
